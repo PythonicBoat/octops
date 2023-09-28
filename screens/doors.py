2f145fa9d32efca137504e1b4f8b3b7913adab54
Dhruv Agarwal <113941899+dhruvspeedcuber@users.noreply.github.com>
2023-09-28 23:54:36 +0530

    fixes main menu icons

--- screens/doors.py
+++ screens/doors.py
@@ -1,6 +1,7 @@
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 from kivy.core.audio import SoundLoader
+from .screen_check import passcode
 
 Builder.load_file('screenLayout/doors_screen.kv')
 
@@ -18,5 +19,11 @@ class DoorsScreen(Screen):
         else:
             label_index.text = f"{int(label_index.text) - 1}"    
 
-    
-
+    def verify(self):
+        code = ""
+        code = self.ids.label_1.text + self.ids.label_2.text + self.ids.label_3.text + self.ids.label_4.text
+        print(code)
+        if passcode == int(code):
+            print(True)
+        else:
+            print(False)
