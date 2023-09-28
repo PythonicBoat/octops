86a665427f08948f155bf509daf0528995bb4576
Anurag Prasoon <anuragprasoon23@gmail.com>
2023-09-29 01:22:10 +0530

    Update spawn.py

--- screens/spawn.py
+++ screens/spawn.py
@@ -6,17 +6,11 @@ import random
 Builder.load_file('screenLayout/spawn_screen.kv')
 
 class SpawnScreen(Screen):
-    def generate_random_numbers(self):
-        # Generate 3 random numbers between 0 and 1
-        random_numbers = [random.random() for _ in range(3)]
-        return random_numbers
 
     def check_slider_values(self):
-        random_numbers = self.generate_random_numbers()
         # Check if each slider value matches the corresponding random number
-        print(round(self.ids.slide1.value, 2))
-        print(round(0.8, 2))
-        if round(self.ids.slide1.value, 2) == round(0.8, 2) and round(self.ids.slide2.value, 2) == round(0.6, 2) and round(self.ids.slide3.value, 2) == round(0.4, 2):
+        print(self.ids.slide1.value)
+        if ((self.ids.slide1.value < 80) and (self.ids.slide1.value > 70 )) and ((self.ids.slide2.value < 60)and ( self.ids.slide2.value > 50 )) and ((self.ids.slide3.value < 100) and (self.ids.slide3.value > 90 )):
             print("Mission successful!")
         else:
             print("Mission failed. Try again.")
