e5e9aba385f82bcba81323dacfea434272440e95
Sasquatch0908 <89185962+SourasishBasu@users.noreply.github.com>
2023-09-29 00:07:57 +0530

    Updated Site_A

--- screens/siteA.py
+++ screens/siteA.py
@@ -2,35 +2,44 @@ from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 from kivy.core.audio import SoundLoader
 
-
 Builder.load_file('screenLayout/sitea_screen.kv')
 sound = SoundLoader.load('assets/audio/sitea_planted.wav')
 
 class SiteAScreen(Screen):
     pressed_buttons = []
 
+    def show_overlay(self):
+        self.ids.overlay.opacity = 1
+        self.pressed_buttons = []
+
     def change_img(self, button, idOG):
         idOG_dynamic = f"{idOG}"
         widget = getattr(self.ids, idOG_dynamic, None)
-        widget.source = "assets/img/symbols/off.png" if widget.source == f"assets/img/symbols/on.png" else f"assets/img/symbols/on.png"
+        widget.source = "assets/img/symbols/off.png" if widget.source == "assets/img/symbols/on.png" else "assets/img/symbols/on.png"
+        print(f"{idOG_dynamic}:{widget.source}")
 
     def check_product(self, instance, id):
         button_id = id  # Get the ID of the pressed button
         self.pressed_buttons.append(id)  # Add the ID to the list
         print(f'Button pressed: {button_id}')
-
+        x = ""
         # Check if we have pressed 4 buttons
         if len(self.pressed_buttons) == 4:
             product = 1
             for button_id in self.pressed_buttons:
-                # Extract the button number from the ID
+                # Multiply the button number to a product var
                 product *= button_id
 
             if product == 70:
-                print("Valid")
+                x = "Valid"
                 sound.play()
+                self.show_overlay()
             else:
-                print("Invalid")
-            self.pressed_buttons = []  # Reset the list for the next set of button presses
+                x = "Invalid"
 
+        if x == "Invalid":
+            for button in self.pressed_buttons:
+                widget = getattr(self.ids, f"{button}", None)
+                widget.source = "assets/img/symbols/off.png"
+            self.pressed_buttons = []
 
