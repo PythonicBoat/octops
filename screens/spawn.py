31e9180c334aa3a984738741b07b985347df392b
Anurag Prasoon <anuragprasoon23@gmail.com>
2023-09-29 13:57:48 +0530

    Update spawn.py

--- screens/spawn.py
+++ screens/spawn.py
@@ -2,6 +2,7 @@ from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 from kivy.uix.slider import Slider
 import random
+from .screen_check import slider
 
 Builder.load_file('screenLayout/spawn_screen.kv')
 
@@ -10,7 +11,10 @@ class SpawnScreen(Screen):
     def check_slider_values(self):
         # Check if each slider value matches the corresponding random number
         print(self.ids.slide1.value)
-        if ((self.ids.slide1.value < 80) and (self.ids.slide1.value > 70 )) and ((self.ids.slide2.value < 60)and ( self.ids.slide2.value > 50 )) and ((self.ids.slide3.value < 100) and (self.ids.slide3.value > 90 )):
+        x=slider[0]
+        y=slider[1]
+        z=slider[2]
+        if ((self.ids.slide1.value < x) and (self.ids.slide1.value > x-10 )) and ((self.ids.slide2.value < y)and ( self.ids.slide2.value > y-10 )) and ((self.ids.slide3.value < z) and (self.ids.slide3.value > z-10 )):
             print("Mission successful!")
         else:
             print("Mission failed. Try again.")
