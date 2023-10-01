bfe1325f4e2f6bcbc1038f53983c7c9b4ee27d2a
Akshat <82810411+Afterdie@users.noreply.github.com>
2023-10-01 20:44:47 +0530

    added hints

--- screens/siteB.py
+++ screens/siteB.py
@@ -13,12 +13,20 @@ sound = SoundLoader.load('assets/audio/siteb_defused.wav')
 
 class SiteBScreen(Screen):
     defused = False
+    visited = False
     
     @staticmethod
     def Delay(delay):
         return Animation(duration=delay)
 
     def on_enter(self):
+        if not SiteBScreen.visited:
+            SiteBScreen.visited = True
+            octocat_icon = self.Delay(0.5) + Animation(color=(1,1,1,1), pos= (dp(0), dp(0)), duration=0.2, t='out_quad') + self.Delay(3.5) + Animation(color=(1,1,1,0), duration=0.2, t='out_quad')
+            octocat_icon.start(self.ids.octocat_icon)
+            text_bubble = self.Delay(1) + Animation(color=(1,1,1,1), pos = (dp(233), dp(155)), duration=0.2, t='out_quad') + self.Delay(3.5) + Animation(color=(1,1,1,0), pos = (dp(233), dp(155)), duration=0.2, t='out_quad')
+            text_bubble.start(self.ids.octocat_hint)
+
         if get_spawn() and get_sitea() and get_doors() and not SiteBScreen.defused:
             self.ids.defuse_img.color = (1,1,1,1)
 
